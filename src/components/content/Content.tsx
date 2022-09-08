291ec23e095d8c1cf22ad488b0bbc2794e962785
Mother-investment <yurii.antipov01@gmail.com>
2022-09-08 12:42:39 +0700

    cartPage

--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import HomePage from './homePage/HomePage'
 import './content.css'
+import CartPage from './cartPage/CartPage';
 
 const Content = () => {
     return (
         <div className='content'>
-            <HomePage />
+            <CartPage />
         </div>
     )
 }
